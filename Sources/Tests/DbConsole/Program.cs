c30ad0852f667a12ba28485002896cb8900c8a5e
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-02-12 16:40:29 +0100

    :sparkles: ajout table player + creation bdd sqLite

--- Sources/Tests/DbConsole/Program.cs
+++ Sources/Tests/DbConsole/Program.cs
@@ -11,10 +11,21 @@ PlayerEntity p1 = new PlayerEntity
     NbPoints = 0
 };
 
+PlayerEntity p2 = new PlayerEntity
+{
+    Pseudo = "Theo",
+    NbWin = 0,
+    NbPlayed = 0,
+    MaxZone = 0,
+    MaxPoints = 0,
+    NbPoints = 0
+};
+
 using (var context = new SQLiteContext())
 {
     Console.WriteLine("Create and Insert new Champion");
     context.Add(p1);
+    context.Add(p2);
     context.SaveChanges();
 }
 
