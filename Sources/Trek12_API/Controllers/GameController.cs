a81d7c6c239c13be29aa9d905d33e110395dc8c1
Aurelien PINTRAND <Aurelien.PINTRAND@etu.uca.fr>
2023-03-20 10:59:54 +0100

    :poop: début GameController, mais error sur le stubData.Games avec les public je sais pas pourquoi je cherche là

---
+++ Sources/Trek12_API/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stub;
+
+namespace Trek12_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController: ControllerBase
+    {
+        private readonly ILogger<GameController> _logger;
+        private StubData.GamesManager gamesManager { get; set; } = new StubData.GamesManager(new StubData());
+
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/AllGames")]
+        public async Task<IActionResult> Get()
+        {
+            var list = await gamesManager
+        }
+    }
+}
