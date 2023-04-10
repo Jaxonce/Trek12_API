8619817ac5da35d1817ea6803d063a00d140e7b9
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-04-10 20:30:15 +0200

    Reajout de dbdatamanager

---
+++ Sources/DbDataManager/DbDataManager.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+
+namespace DbDataManager;
+public class DbDataManager : IDataManager
+{
+    public IPlayersManager PlayersMgr => throw new NotImplementedException();
+
+    public IGamesManager GamesMgr => throw new NotImplementedException();
+
+    public IGamesModeManager GamesModeMgr => throw new NotImplementedException();
+
+    public ICasesManager CasesMgr => throw new NotImplementedException();
+
+    public IGrillesManager GrillesMgr => throw new NotImplementedException();
+
+    public IStatsManager StatsMgr => throw new NotImplementedException();
+
+    public ITurnsManager TurnsMgr => throw new NotImplementedException();
+}
+
